6395ba9dcbe19479cbc504216da6925bfb3a0652
[name] <69193208+manthanoice@users.noreply.github.com>
2021-11-23 18:58:35 +0530

    eh

--- Day-1/band-name-gen.py
+++ Day-1/band-name-gen.py
@@ -5,5 +5,4 @@ answer = s1+" "+s2
 print("Your band name could be {}".format(answer))
 
 
-#Well I know python already but just want to do 
-# everything from scratch properly, so here I am!
+#Well I know python already but just want to do everything from scratch properly, so here I am!
