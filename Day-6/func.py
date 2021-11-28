17b4f2596a5e125b48ff88e1c1cf105baed438eb
[name] <69193208+manthanoice@users.noreply.github.com>
2021-11-28 22:46:59 +0530

    add day-6

---
+++ Day-6/func.py
@@ -0,0 +1,11 @@
+def sum(a, b):
+    return int(a+b)
+def max(a, b):
+    if a>b:
+        print("{} is bigger than {}".format(a, b))
+    else:
+        print("{} is bigger than {}".format(b, a))
+max(5,6)
+print("The sum is: {}".format(sum(49,324)))
+
+#And today most of the work was done online and about while loop, so not much here, we move! Lessgo!
