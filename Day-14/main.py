3d6961c35b6a6b9e8998ec70605642f9460cdef4
[name] <69193208+manthanoice@users.noreply.github.com>
2021-12-06 15:03:21 +0530

    add day-14

---
+++ Day-14/main.py
@@ -0,0 +1,34 @@
+from game_data import the_data
+import random
+from art import the_logo
+from art import vs
+from replit import clear
+
+def game():
+    score = 0
+    num_1 = random.randint(0, 49)
+    choice_a = the_data[num_1]
+    should_continue = True
+    while should_continue:
+        print(the_logo)
+        print("Your score is {}".format(score))
+        num_2 = random.randint(0, 49)
+        if num_1 == num_2:
+            num_2 = random.randint(0, 49)
+        choice_b = the_data[num_2]
+        print("Compare A: {}, a {}, from {}!".format(choice_a['name'], choice_a['description'], choice_a['country']))
+        print(vs)
+        print("Against B: {}, a {}, from {}!".format(choice_b['name'], choice_b['description'], choice_b['country']))
+        your_guess = input("Who has more followers? Type 'A' or 'B': ").upper()
+        if your_guess == 'A' and choice_a['follower_count'] > choice_b['follower_count']:
+            score+=1
+            choice_a = choice_a
+            clear()
+        elif your_guess == 'B' and choice_b['follower_count'] > choice_a['follower_count']:
+            score+=1
+            choice_b = choice_a
+            clear()
+        else:
+            print("Oops Wrong answer! You lost! :( and your final Score is {}".format(score))
+            should_continue = False
+game()
