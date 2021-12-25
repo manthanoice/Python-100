48ef567425e77c4b0825e2fed6e07b3061024b11
[name] <69193208+manthanoice@users.noreply.github.com>
2021-12-25 22:10:53 +0530

    add

---
+++ Day-32/main.py
@@ -0,0 +1,11 @@
+import smtplib
+import datetime as dt
+import random
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 5:
+    with open('Day-32\quotes.txt', encoding='utf8') as file:
+        all_quotes = file.readlines()
+        quote = random.choice(all_quotes)
+    print(quote)
