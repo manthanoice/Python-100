370cf204e0486def33cc9d35b13bdaa09ebaf050
[name] <69193208+manthanoice@users.noreply.github.com>
2021-12-28 18:46:53 +0530

    add

--- Day-33/sun.py
+++ Day-33/sun.py
@@ -1,21 +1,43 @@
 import requests
 from datetime import datetime
+import time
+import smtplib
 
 the_params = {
     'lat' : 22.2587,
     'lng' : 71.1924,
     'formatted': 0
 }
+def is_night():
+    the_request = requests.get(url='https://api.sunrise-sunset.org/json', params=the_params)
+    the_request.raise_for_status()
+    data = the_request.json()
+    sunrise = int(data['results']['sunrise'].split('T')[1].split(':')[0])
+    sunset = int(data['results']['sunset'].split('T')[1].split(':')[0])
 
-the_request = requests.get(url='https://api.sunrise-sunset.org/json', params=the_params)
-the_request.raise_for_status()
-data = the_request.json()
+    print(sunrise)
+    print(sunset)
 
-sunrise = data['results']['sunrise'].split('T')[1].split(':')[0]
-sunset = data['results']['sunset'].split('T')[1].split(':')[0]
+    time_now = datetime.now().hour
+    if time_now >= sunset and time_now <= sunrise:
+        return True
 
-print(sunrise)
-print(sunset)
+def is_is_visible():
+    the_request = requests.get(url='http://api.open-notify.org/iss-now.json')
+    the_request.raise_for_status()
+    data = the_request.json()
 
-time_now = datetime.now()
-print(time_now.hour)
+    iss_lat = float(data['iss_position']['latitude'])
+    iss_lng = float(data['iss_position']['longitude'])
+
+    lat = the_params['lat'] - 5
+    lng = the_params['lng'] - 5
+    if lat <= iss_lat <= lat+10 and lng <= iss_lng <= lng+10:
+        return True
+
+while True:
+    time.sleep(6.0)
+    if is_is_visible() and is_night():
+        connection = smtplib.SMTP('smtp.gmail.com')
+        connection.starttls()
+        connection.login('email', 'pass')
