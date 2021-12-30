1a5fdf761867a027bb6b7433a4a6db0896ba0584
[name] <69193208+manthanoice@users.noreply.github.com>
2021-12-30 21:01:43 +0530

    add

--- Day-34/ui.py
+++ Day-34/ui.py
@@ -6,6 +6,7 @@ THEME_COLOR = "#375362"
 class QuizInterface:
 
     def __init__(self, quiz_brain: QuizBrain):
+        #param
         self.quiz = quiz_brain
 
         #windows
@@ -33,15 +34,38 @@ class QuizInterface:
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         #buttons using image
-        self.true_button = Button(image = self.true, highlightthickness=0)
-        self.false_button = Button(image = self.false, highlightthickness=0)
+        self.true_button = Button(image = self.true, highlightthickness=0, command=self.true_pressed)
+        self.false_button = Button(image = self.false, highlightthickness=0, command=self.false_pressed)
         self.true_button.grid(row=2, column=0)
         self.false_button.grid(row=2, column=1)
 
+        #To replace question text in GUI
         self.the_next_question()
+
         #ending
         self.windows.mainloop()
     
+    #For the next question
     def the_next_question(self):
-        the_question_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text = the_question_text)
+        self.canvas.config(bg='white')
+        if self.quiz.still_has_questions():
+            self.score_label.config(text='Score: {}'.format(self.quiz.score))
+            the_question_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text = the_question_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text = 'Hello user, you have reached the end of the Quiz!')
+            self.true_button.config(state='disable')
+            self.false_button.config(state='disable')
+    
+    def true_pressed(self):
+        self.give_feedback(is_right=self.quiz.check_answer('True'))
+
+    def false_pressed(self):
+        self.give_feedback(is_right=self.quiz.check_answer('False'))
+    
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg='green')
+        else:
+            self.canvas.config(bg='red')
+        self.windows.after(1000, self.the_next_question)
---
+++ Day-34/wut.py
@@ -0,0 +1,7 @@
+def age_check(age: int) -> bool:
+    if age > 12:
+        return True
+    else:
+        return False
+
+print(age_check(123))
