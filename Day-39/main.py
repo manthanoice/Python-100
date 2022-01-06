81576a6d54594aa772e7042f5e4e44ce2b93fc52
[name] <69193208+manthanoice@users.noreply.github.com>
2022-01-06 20:16:15 +0530

    add

--- Day-39/main.py
+++ Day-39/main.py
@@ -5,8 +5,16 @@
 
 # data_manager.get_city_code('Paris')
 
+from typing import final
 import requests
 
 response = requests.get(url='https://api.sheety.co/8cd035ce8e01d3ce4df2b6f8ff6b0ea4/flightApi/sheet1')
 data = response.json()['sheet1']
-print(data)
+
+response_2 = requests.get(url='https://tequila-api.kiwi.com/locations/query?term=PRG&locale=en-US&location_types=airport&limit=10&active_only=true')
+print(response_2)
+
+for i in range(len(data)):
+    the_data = data[i]
+    if the_data['city'] == 'Paris':
+        print(the_data['iataCode'])
