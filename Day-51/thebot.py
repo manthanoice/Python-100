48dab69e1a62a245dd8ebe7caddb7a4997f6f8be
[name] <69193208+manthanoice@users.noreply.github.com>
2022-01-19 17:32:04 +0530

    add

--- Day-51/thebot.py
+++ Day-51/thebot.py
@@ -34,18 +34,14 @@ class InternetSpeedTwitterBot():
         self.driver.get(SPEEDTEST)
         self.go = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a/span[4]')
         self.go.click()
-
         #wait for maybe 60 seconds until the speed test is done
         time.sleep(60)
-
         #getting the download speed
         self.down = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span')
         self.download_speed = self.down.text
-
         #getting the upload speed
         self.up = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span')
         self.upload_speed = self.up.text
-
         #printing them in console
         print(self.download_speed)
         print(self.upload_speed)
@@ -62,52 +58,34 @@ class InternetSpeedTwitterBot():
             #signing in
             self.sign_in = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/div[3]/div[5]/a/div')
             self.sign_in.click()
-
             time.sleep(2)
-
             #entering username
             self.the_username_input = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[5]/label/div/div[2]/div/input')
             self.the_username_input.click()
             self.the_username_input.send_keys(MYUSERNAME)
-
             self.next_step = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[6]/div/span/span')
             self.next_step.click()
-
             time.sleep(1)
-
             #then entering password
             self.password_entering = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[3]/div/label/div/div[2]/div[1]/input')
             self.password_entering.send_keys(MYPASSWORD)
             self.password_entering.send_keys(Keys.ENTER)
-
             time.sleep(5)
-
             #making the tweet
             self.tweeting = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/label/div[1]/div/div/div/div/div[2]/div/div/div/div')
             self.tweeting.click()
-
-            self.message = 'Hey internet service provider, why my internet speed currently is {} down and {} up when we had deal of having {} down and {} up'.format(self.download_speed, self.upload_speed, EXPECTED_DOWNLOAD_SPEED, EXPECTED_UPLOAD_SPEED)
-
+            self.message = '[Bot test] Hey internet service provider, why my internet speed currently is {} down and {} up when we had deal of having {} down and {} up'.format(self.download_speed, self.upload_speed, EXPECTED_DOWNLOAD_SPEED, EXPECTED_UPLOAD_SPEED)
             self.tweeting.send_keys(self.message)
-
             #sending the tweet
             self.send_tweet = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[3]/div/div/div[2]/div/div/span/span')
-
             self.send_tweet.click()
-
             #finding my profile
             self.my_profile = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/header/div/div/div/div[2]/div/div/div/div/div[2]/div/div[2]/div/div/div[4]/div')
-
             self.my_profile.click()
-
             #finding log out option from my profile and then logging out
             self.log_out = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div[2]/div/div/div[2]/div/div[2]/div/div/div/div/div/a[2]/div[1]/div')
-
             self.log_out.click()
-
             self.final_log_out = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div[2]/div[1]/div/span/span')
-
             self.final_log_out.click()
-            #well I guess we're done
         else:
             pass
