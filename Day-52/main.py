fa8ddb339a4196550d473bb6f02708c857cc0c64
[name] <69193208+manthanoice@users.noreply.github.com>
2022-01-19 22:18:13 +0530

    add instagram bot :P

--- Day-52/main.py
+++ Day-52/main.py
@@ -1,10 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementClickInterceptedException
 from idPass import *
 import time
-
-#ToDo - Complete the methods after eating :P
 
 CHROME_DRIVER_PATH = 'C:\Development\chromedriver.exe'
 SIMILAR_ACCOUNT = 'officialfootballcommunity'
@@ -19,6 +18,7 @@ class InstaFollower():
     def login(self):
         '''to log into your instagram account'''
         self.driver.get(url='https://www.instagram.com/')
+        self.driver.maximize_window()
         time.sleep(2)
         self.login = self.driver.find_element(By.NAME, 'username')
         self.login.send_keys(MY_USERNAME)
@@ -33,10 +33,26 @@ class InstaFollower():
         time.sleep(4)
         self.follower = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a')
         self.follower.click()
+        time.sleep(4)
+        page = self.driver.find_element(By.XPATH, '/html/body/div[6]/div/div/div[2]')
+        for i in range(2):
+            self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", page)
+            time.sleep(2)
     
     def follow(self):
         '''To follow the people we found'''
+        all_buttons = self.driver.find_elements(By.CSS_SELECTOR, 'li button')
+        for button in all_buttons:
+            try:
+                button.click()
+                time.sleep(2)
+                #just to be sure :P gonna print a little :PPPP
+                print('followed')
+            except ElementClickInterceptedException:
+                cancel_button = self.driver.find_element_by_xpath("/html/body/div[7]/div/div/div/div[3]/button[2]")
+                cancel_button.click()
 
 bot = InstaFollower()
 bot.login()
 bot.find_followers()
+bot.follow()
