64cb2b937a45ff487dbb4584a10eb820a32c2c20
[name] <69193208+manthanoice@users.noreply.github.com>
2022-01-20 20:35:30 +0530

    add today

---
+++ Day-53/data.py
@@ -0,0 +1,17 @@
+from os import urandom
+from bs4 import BeautifulSoup
+import requests
+
+header = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36',
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+}
+
+my_response = requests.get(url='https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3Anull%2C%22mapBounds%22%3A%7B%22west%22%3A-122.69219435644531%2C%22east%22%3A-122.17446364355469%2C%22south%22%3A37.703343724016136%2C%22north%22%3A37.847169233586946%7D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A11%7D', headers=header)
+
+soup = BeautifulSoup(my_response.text, 'html.parser')
+
+price = soup.select('.list-card-price')
+for i in price:
+    the_text = i.getText().strip('bd').replace('+','').replace('/mo','')[0:-3]
+    print(the_text)
